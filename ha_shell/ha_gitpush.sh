335ba1e78d68d2c4973c9920e5f9a246c910e74e
Brian McCarty <bkmccarty@gmail.com>
2021-07-18 20:29:29 -0700

    config files on 18-07-2021 20:29:29

--- ha_shell/ha_gitpush.sh
+++ ha_shell/ha_gitpush.sh
@@ -2,6 +2,9 @@
 # Change this to your Home Assistant config folder if it is different
 cd /config
 # Add all files to the repository with respect to .gitignore rules
+git config --global user.email "bkmccarty@gmail.com"
+git config --global user.name "Brian McCarty"
+
 git add .
 # Commit changes with message with current date stamp
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
